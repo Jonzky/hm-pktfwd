2a36303c46ca2aff6162353428ff33bcdcde558c
Jon Zakay <JonZakay@aol.com>
2022-01-23 16:24:29 +0000

    Fix wget output path

--- start.sh
+++ start.sh
@@ -7,10 +7,11 @@ if [[ -z "${OVERRIDE_PKT_CONFIG_URL}" ]]; then
   echo "No custom config has been passed, so it will use the default one"
   cp /opt/pktfwd/config/lora_templates_sx1302/EU-global_conf.json $CONFIG
 else
-  wget -O "/opt/sx1302/config.json" \
+  wget -O $CONFIG \
       "${OVERRIDE_PKT_CONFIG_URL}"
 fi
 
+cp /opt/pktfwd/config/lora_templates_sx1302/local_conf.json /opt/local_conf.json
 cp /opt/pktfwd/config/lora_templates_sx1302/local_conf.json /opt/local_conf.json
 
 /opt/sx1302/lora_pkt_fwd -c $CONFIG
