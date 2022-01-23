7f87d1686d6cd0f5fa138a18bc5e35919a291ca4
Jon Zakay <JonZakay@aol.com>
2022-01-23 15:06:35 +0000

    Fix config var

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-CONFIG="/opt/sx1302_hal/packet_forwarder/global_conf.json.sx1250.EU868"
+CONFIG="/opt/config.json"
 
 # If a url has been set use it, otherwise use the default
 if [[ -z "${OVERRIDE_PKT_CONFIG_URL}" ]]; then
   echo "No custom config has been passed, so it will use the default one"
-  cp /opt/pktfwd/config/lora_templates_sx1302/EU-global_conf.json /opt/sx1302/config.json
+  cp /opt/pktfwd/config/lora_templates_sx1302/EU-global_conf.json /opt/config.json
 else
   wget -O "/opt/sx1302/config.json" \
-      "${OVERRIDE_CONFIG_URL}"
+      "${OVERRIDE_PKT_CONFIG_URL}"
 fi
 
 /opt/sx1302/lora_pkt_fwd -c $CONFIG
