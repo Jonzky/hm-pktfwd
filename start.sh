d79893cff6663dd065997ae906656d7f5f161683
Jon Zakay <JonZakay@aol.com>
2022-01-23 15:35:40 +0000

    Fix file permissions

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ CONFIG="/opt/config.json"
 # If a url has been set use it, otherwise use the default
 if [[ -z "${OVERRIDE_PKT_CONFIG_URL}" ]]; then
   echo "No custom config has been passed, so it will use the default one"
-  cp /opt/pktfwd/config/lora_templates_sx1302/EU-global_conf.json /opt/config.json
+  cp /opt/pktfwd/config/lora_templates_sx1302/EU-global_conf.json $CONFIG
 else
   wget -O "/opt/sx1302/config.json" \
       "${OVERRIDE_PKT_CONFIG_URL}"
